7b1bf13a8b6d5fd316f7f81bd6c6d108f4e7531c
Malloc-Luo <2423049596@qq.com>
2019-09-08 23:45:55 +0800

    test file

--- ArduinoPart/ArduinoPart.ino
+++ ArduinoPart/ArduinoPart.ino
@@ -1,4 +1,8 @@
-char data;
+typedef unsigned char DATA;
+
+DATA send_data;
+DATA rx_data;
+int message;
 
 void setup() 
 {
@@ -9,8 +13,12 @@ void loop()
 {
   if(Serial.available()>0)
   {
-    data=Serial.read();
-    println(data);
+    rx_data=Serial.read();
+    Serial.println(rx_data);
+    message=Serial.write("I have recived you message");
+    delay(5);
   }
-  
+  send_data = rx_data;
+  Serial.write(send_data);
+  delay(5);
 }
