5c11634a48b1a1899be69bda167c69c5d29280ee
Malloc-Luo <2423049596@qq.com>
2019-09-20 20:20:55 +0800

    save

--- ArduinoPart/ArduinoPart.ino
+++ ArduinoPart/ArduinoPart.ino
@@ -1,37 +1,29 @@
 
-typedef unsigned int DATA;
+typedef unsigned short DATA;
 
-int state=HIGH;
 DATA send_data=15;
 DATA rx_data;
+
+char compare[]="abcd";
+char comdata[]="";
+
+int i=0;
+int t=0;
+int mark;
 
 
 void setup() 
 {
   Serial.begin(9600);
   pinMode(13,OUTPUT);
-  digitalWrite(13,state);
+  
 }
 
 void loop() 
 {
-  #if 0
-  Serial.print(15);
-  if(Serial.available()>0)
-  {
-    rx_data=Serial.read();
-    Serial.println(rx_data);
-    Serial.println("a");
-    if(rx_data==0x01||rx_data==0x02)
-    {
-      state=!state;
-      digitalWrite(13,state);
-      Serial.println("success");
-    }
-    delay(10);
-  }
-  #endif
 
+  #if 0
+  
   digitalWrite(13,HIGH);
   delay(100);
   Serial.print("abcd\r\n");
@@ -39,5 +31,60 @@ void loop()
   digitalWrite(13,LOW);
   delay(100);
   
-  
+  #endif
+
+  #if 0
+
+  if(Serial.available())
+  {
+    String comdata="";
+    while(Serial.available()>0)
+    {
+      if(char(Serial.read()!='\r'&&Serial.read()!='\n'))
+      {
+        comdata=comdata+char(Serial.read());
+        delay(2);
+      }
+    }
+    i=0;
+    for(t=0;t<comdata.length();t++)
+    {
+      if(comdata[i]==compare[i])
+      {
+        mark=1;
+      }
+      else
+      {
+        mark=0;
+        break;
+      }
+      i++;
+    }
+    
+  }
+  if(mark=1)
+  {
+    digitalWrite(13,HIGH);
+    delay(500);
+    digitalWrite(13,LOW);
+    delay(500);
+  }
+  #endif
+
+  if(Serial.available())
+  {
+    rx_data=Serial.read();
+    if(rx_data==15)
+    {
+      mark=1;
+    }
+    else mark=0;
+  }
+  if(mark==1)
+  {
+    digitalWrite(13,HIGH);
+    delay(200);
+    digitalWrite(13,LOW);
+    delay(200);
+  }
 }
